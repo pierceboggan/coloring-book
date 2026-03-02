b898238ba9e8a3a2804720d930a73e9f301ba196
Pierce Boggan <pierceboggan14@gmail.com>
2026-03-02 14:59:42 -0700

    auto-commit: 2026-03-02 14:59:42

--- ios/generate-app-icons.js
+++ ios/generate-app-icons.js
@@ -2,6 +2,20 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
+/*
+  iOS App Icon Generator
+
+  Usage:
+  1. Place a 1024x1024 PNG image named 'app-icon.png' in the ios/ directory
+  2. Run: node generate-app-icons.js
+  3. All required iOS icon sizes will be generated in AppIcon.appiconset/
+
+  To generate new app icons:
+  - Use AI image generation or create a 1024x1024 PNG
+  - Save as ios/app-icon.png
+  - Run this script
+*/
+
 const iconSizes = [
   { name: 'Icon-60@2x.png', size: 120 },
   { name: 'Icon-60@3x.png', size: 180 },
@@ -11,7 +25,7 @@ const iconSizes = [
   { name: 'Icon-1024.png', size: 1024 }
 ];
 
-const sourceImage = path.join(__dirname, 'ColoringBook/ColoringBook/Assets.xcassets/AppIcon.appiconset/icons/app-icon-1024.png');
+const sourceImage = path.join(__dirname, 'app-icon.png');
 const outputDir = path.join(__dirname, 'ColoringBook/ColoringBook/Assets.xcassets/AppIcon.appiconset');
 
 async function generateIcons() {
